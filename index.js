87eb41f3c9b4ec782741feca95c3100d248253d2
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-02 03:46:17 +0530

    changed file

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on http://localhost:3000');
+server.listen(5000, () => {
+  console.log('Server listening on http://localhost:5000');
 });
