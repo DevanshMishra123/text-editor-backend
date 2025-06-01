ed1f01bb2883bfe8fe562d12391f9c4cf4974f43
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-02 03:40:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors({
+  origin: '*',  
+  methods: ['GET', 'POST'],        
+}))
+const server = http.createServer(app);
+const io = new Server(server); 
+
+app.get('/', (req, res) => {
+  res.send("Editor server is running")
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('cursor-moved', (msg) => {
+    io.broadcast.emit('cursor-moved', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on http://localhost:3000');
+});
