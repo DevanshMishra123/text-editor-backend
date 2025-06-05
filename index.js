b3d10e07d7a9f90679c144d1c3cbddc6f5c317e6
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-05 23:29:01 +0530

    changed files

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ app.get('/', (req, res) => {
 
 const server = http.createServer(app);
 
+let currentText = "";
+
 const io = new Server(server, {
   cors: {
-    origin: "https://text-editor-bay-zeta.vercel.app", 
+    origin: "https://text-editor-bay-zeta.vercel.app",
     methods: ["GET", "POST"]
   }
 });
@@ -21,13 +23,25 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
+  socket.emit('init', currentText);
+
+  socket.broadcast.emit('user-joined', { userId: socket.id });
+
   socket.on('cursor-moved', (msg) => {
     console.log(msg);
+
+    if (msg.operation === "add") {
+      currentText = currentText.slice(0, msg.cursor) + msg.text + currentText.slice(msg.cursor);
+    } else if (msg.operation === "delete") {
+      currentText = currentText.slice(0, msg.cursor) + currentText.slice(msg.cursor + msg.text.length);
+    }
+
     socket.broadcast.emit('cursor-moved', msg);
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
+    socket.broadcast.emit('user-left', { userId: socket.id });
   });
 });
 
